0229b1d81268419d5e20b524d2067753d3bc0869
IT21197482 <99175667+IT21197482@users.noreply.github.com>
2023-05-06 23:00:50 +0530

    Add files via upload

---
+++ FlexPay/app/src/main/java/com/example/loginpageui/MainActivity.kt
@@ -0,0 +1,16 @@
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btn
